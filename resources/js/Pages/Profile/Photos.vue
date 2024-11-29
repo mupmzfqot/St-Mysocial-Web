e46fecadb6679703da472ebdbf99be7854c2a1ed
Julpa Denaldi <denaldi.j@gmail.com>
2024-11-29 09:29:58 +0700

    update profile image

--- resources/js/Pages/Profile/Photos.vue
+++ resources/js/Pages/Profile/Photos.vue
@@ -19,6 +19,16 @@ watch(
         (q) => router.get(route('post.index'), { search: q }, { preserveState: true }), 500
     )
 );
+
+const setActive = (user) => {
+
+}
+
+const reject = (user) => {
+
+}
+
+
 </script>
 
 <template>
@@ -102,45 +112,46 @@ watch(
                             </thead>
 
                             <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
-<!--                            <tr v-for="post in posts.data" key="user.id" class="bg-white hover:bg-gray-50 dark:bg-neutral-900 dark:hover:bg-neutral-800">-->
-<!--                                <td class="size-px whitespace-nowrap align-top">-->
-<!--                                    <a class="block p-6" href="#">-->
-<!--                                        <span class="text-sm text-gray-600 dark:text-neutral-400">{{ post.author.name }}</span>-->
-<!--                                    </a>-->
-<!--                                </td>-->
-<!--                                <td class="size-px whitespace-nowrap align-top">-->
-<!--                                    <a class="block p-6" href="#">-->
-<!--                                        <span class="text-sm text-gray-600 dark:text-neutral-400">{{ post.post }}</span>-->
-<!--                                    </a>-->
-<!--                                </td>-->
-<!--                                <td class="size-px whitespace-nowrap align-top">-->
-<!--                                    <a class="block p-6" href="#">-->
-<!--                                        <span class="text-sm text-gray-600 dark:text-neutral-400">{{ post.created_at }}</span>-->
-<!--                                    </a>-->
-<!--                                </td>-->
+                            <tr v-for="photo in photos.data" key="user.id" class="bg-white hover:bg-gray-50 dark:bg-neutral-900 dark:hover:bg-neutral-800">
+                                <td class="size-px whitespace-nowrap align-top">
+                                    <a class="block p-6" href="#">
+                                        <span class="text-sm text-gray-600 dark:text-neutral-400">{{ photo.name }}</span>
+                                    </a>
+                                </td>
+                                <td class="size-px whitespace-nowrap align-top">
+                                    <a class="block p-6" href="#">
+                                        <img :src="photo.avatar" class="object-scale-down size-20" alt="photo">
+                                    </a>
+                                </td>
+                                <td class="size-px whitespace-nowrap align-top">
+                                    <a class="block p-6" href="#">
+                                        <span class="text-sm text-gray-600 dark:text-neutral-400">{{ photo.media[0].created_at }}</span>
+                                    </a>
+                                </td>
 
-<!--                                <td class="size-px whitespace-nowrap align-top">-->
-<!--                                    <div class="p-6">-->
-<!--                                        <div class="hs-dropdown [&#45;&#45;placement:bottom-right] relative inline-block">-->
-<!--                                            <button id="hs-table-dropdown-1" type="button" class="hs-dropdown-toggle py-1.5 px-2 inline-flex justify-center items-center gap-2 rounded-lg text-gray-700 align-middle disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:text-neutral-400 dark:hover:text-white dark:focus:ring-offset-gray-800" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">-->
-<!--                                                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>-->
-<!--                                            </button>-->
-<!--                                            <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden divide-y divide-gray-200 min-w-40 z-20 bg-white shadow-2xl rounded-lg p-2 mt-2 dark:divide-neutral-700 dark:bg-neutral-800 dark:border dark:border-neutral-700" role="menu" aria-orientation="vertical" aria-labelledby="hs-table-dropdown-1">-->
-<!--                                                <div class="py-2 first:pt-0 last:pb-0">-->
-<!--                                                    <a href="#" class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-scale-animation-modal" data-hs-overlay="#confirm-dialog">-->
-<!--                                                        Edit-->
-<!--                                                    </a>-->
-<!--                                                </div>-->
-<!--                                                <div class="py-2 first:pt-0 last:pb-0">-->
-<!--                                                    <a href="#" class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-red-600 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-red-500 dark:hover:bg-neutral-700 dark:hover:text-neutral-300" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-scale-animation-modal" data-hs-overlay="#confirm-dialog">-->
-<!--                                                        Remove-->
-<!--                                                    </a>-->
-<!--                                                </div>-->
-<!--                                            </div>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </td>-->
-<!--                            </tr>-->
+                                <td class="size-px whitespace-nowrap align-top">
+                                    <div class="p-6">
+                                        <div class="hs-dropdown [--placement:bottom-right] relative inline-block">
+                                            <button id="hs-table-dropdown-1" type="button" class="hs-dropdown-toggle py-1.5 px-2 inline-flex justify-center items-center gap-2 rounded-lg text-gray-700 align-middle disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:text-neutral-400 dark:hover:text-white dark:focus:ring-offset-gray-800" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                                                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>
+                                            </button>
+                                            <div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden divide-y divide-gray-200 min-w-40 z-20 bg-white shadow-2xl rounded-lg p-2 mt-2 dark:divide-neutral-700 dark:bg-neutral-800 dark:border dark:border-neutral-700" role="menu" aria-orientation="vertical" aria-labelledby="hs-table-dropdown-1">
+                                                <div class="py-2 first:pt-0 last:pb-0">
+                                                    <a @click="setActive(photo)" class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300" href="#" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-scale-animation-modal" data-hs-overlay="#confirm-dialog">
+                                                        Approve
+                                                    </a>
+                                                    <a @click="reject(photo)" class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300" href="#" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-scale-animation-modal" data-hs-overlay="#confirm-dialog">
+                                                        Reject
+                                                    </a>
+                                                    <Link :href="route('user.profile', photo.id)" class="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300" href="#">
+                                                        Go to Account
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
 
                             </tbody>
                         </table>
