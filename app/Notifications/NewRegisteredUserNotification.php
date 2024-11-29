86b5b4cc2b6f164cf1d7c7bcb55b5cc972b87d7d
Julpa Denaldi <denaldi.j@gmail.com>
2024-11-29 07:57:18 +0700

    fix: user registration

---
+++ app/Notifications/NewRegisteredUserNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewRegisteredUserNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Protected User $user)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New User Registration')
+            ->line("New user {$this->user->name} has been registered.");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'id'        => $this->user->id,
+            'name'      => $this->user->name,
+            'message'   => "{$this->user->name} has registered."
+        ];
+    }
+}
---
+++ resources/views/mail/registersuccess.blade.php
@@ -0,0 +1,9 @@
+<x-mail::message>
+# Registration Succeeded
+
+Before you can login, your account must be manually activated by an administrator.<br>
+Please wait, this can take some time.
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
