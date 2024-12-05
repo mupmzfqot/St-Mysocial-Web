6386667dcf67245b3d834188241a94a27ff43ea7
Julpa Denaldi <denaldi.j@gmail.com>
2024-12-06 06:55:20 +0700

    fix: post with media

--- app/Actions/Posts/CreatePost.php
+++ app/Actions/Posts/CreatePost.php
@@ -5,38 +5,54 @@ namespace App\Actions\Posts;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class CreatePost
 {
     public function handle($request, $id = null)
     {
-        $published = (bool) Auth::user()->hasAnyRole(['admin', 'user']);
-        if($request->post('type') == 'public') {
-            $published = false;
-        }
-
-        $data = [
-            'post' => $request->post('content'),
-            'user_id'  => $request->user()->id,
-            'published' => $published,
-            'type'  => $request->post('type'),
-            'location' => $request->post('location'),
-            'feeling' => $request->post('feeling'),
-        ];
-
         DB::beginTransaction();
 
         try {
+
+            $request->validate([
+                'files.*' => [
+                    'mimetypes:image/*,video/mp4,video/*',
+                    'file'
+                ],
+                'content' => 'required',
+            ]);
+
+            $published = (bool) Auth::user()->hasAnyRole(['admin', 'user']);
+            if($request->post('type') == 'public') {
+                $published = false;
+            }
+
+            $data = [
+                'post' => $request->post('content'),
+                'user_id'  => $request->user()->id,
+                'published' => $published,
+                'type'  => $request->post('type'),
+                'location' => $request->post('location'),
+                'feeling' => $request->post('feeling'),
+            ];
+
+
             if($id) {
                 $post = Post::find($id)->update($data);
             } else {
                 $post = Post::query()->create($data);
             }
 
-            if (!empty($request->file('files'))) {
-                foreach ($request->file('files') as $file) {
-                    $post->addMedia($file)
-                        ->toMediaCollection('post_media');
+            if ($request->hasFile('files')) {
+
+                if(is_array($request->file('files'))) {
+                    foreach ($request->files as $file) {
+                        $post->addMedia($file)
+                            ->toMediaCollection('post_media');
+                    }
+                } else {
+                    $post->addMedia($request->file('files'))->toMediaCollection('post_media');
                 }
             }
 
