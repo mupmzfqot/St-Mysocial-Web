5f970cd878a18184a5912cbcfa553d6822e13677
Julpa Denaldi <denaldi.j@gmail.com>
2025-01-02 14:40:19 +0700

    fix: permission filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,6 +33,16 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
             'throw' => false,
         ],
 
@@ -60,6 +70,16 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public/media'),
             'url' => env('APP_URL').'/storage/media',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
             'throw' => false,
         ]
 
