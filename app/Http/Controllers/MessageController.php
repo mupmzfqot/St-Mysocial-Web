063d54bc08a22bf1f71d318f30a48b3c1981c980
Julpa Denaldi <denaldi.j@gmail.com>
2025-01-31 23:03:33 +0700

    fix: hide conversation with no message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -34,6 +34,7 @@ class MessageController extends Controller
             ])
             ->withMax('messages', 'created_at')
             ->orderByDesc('messages_max_created_at')
+            ->whereHas('messages')
             ->get();
 
         $conversations = $results->map(function ($conversation) {
