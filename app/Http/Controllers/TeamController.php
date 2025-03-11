5308c6b8017dc67e1854115f71d4546b859eb762
Julpa Denaldi <denaldi.j@gmail.com>
2025-03-11 12:43:20 +0700

    fix: sort teamst

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -9,19 +9,22 @@ class TeamController extends Controller
 {
     public function get()
 {
-    $cacheKey = 'team_users_' . (auth()->user() ? auth()->user()->id : 'guest');
-    $cacheDuration = now()->addMinutes(5);
+//    $cacheKey = 'team_users_' . (auth()->user() ? auth()->user()->id : 'guest');
+//    $cacheDuration = now()->addMinutes(5);
 
-    return \Cache::remember($cacheKey, $cacheDuration, function () {
-        $query = User::query()
+//    return \Cache::remember($cacheKey, $cacheDuration, function () {
+        $query = User::query()->isActive()
             ->whereHas('roles', function ($query) {
-                $query->where('name', 'user');
+                $query->whereIn('name', ['user', 'admin']);
             })
+            ->withCount(['posts as posts_count' => function ($query) {
+                $query->where('published', 1);
+            }])
             ->whereNotNull('email_verified_at')
-            ->where('is_active', true)
-            ->where('id', '!=', auth()->user()->id);
+            ->where('id', '!=', auth()->user()->id)
+            ->orderBy('posts_count', 'desc');
 
         return $query->get();
-    });
+//    });
 }
 }
