cf134517aa2e9aff07a0d088c150f6240a4354c6
Julpa Denaldi <denaldi.j@gmail.com>
2025-03-16 16:07:27 +0700

    remove admin from teamst lists

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -8,23 +8,23 @@ use Illuminate\Http\Request;
 class TeamController extends Controller
 {
     public function get()
-{
-//    $cacheKey = 'team_users_' . (auth()->user() ? auth()->user()->id : 'guest');
-//    $cacheDuration = now()->addMinutes(5);
+    {
+        $cacheKey = 'team_users_' . (auth()->user() ? auth()->user()->id : 'guest');
+        $cacheDuration = now()->addMinutes(5);
 
-//    return \Cache::remember($cacheKey, $cacheDuration, function () {
-        $query = User::query()->isActive()
-            ->whereHas('roles', function ($query) {
-                $query->whereIn('name', ['user', 'admin']);
-            })
-            ->withCount(['posts as posts_count' => function ($query) {
-                $query->where('published', 1);
-            }])
-            ->whereNotNull('email_verified_at')
-            ->where('id', '!=', auth()->user()->id)
-            ->orderBy('posts_count', 'desc');
+        return \Cache::remember($cacheKey, $cacheDuration, function () {
+                $query = User::query()->isActive()
+                    ->whereHas('roles', function ($query) {
+                        $query->whereIn('name', ['user']);
+                    })
+                    ->withCount(['posts as posts_count' => function ($query) {
+                        $query->where('published', 1);
+                    }])
+                    ->whereNotNull('email_verified_at')
+                    ->where('id', '!=', auth()->user()->id)
+                    ->orderBy('posts_count', 'desc');
 
-        return $query->get();
-//    });
-}
+                return $query->get();
+        });
+    }
 }
