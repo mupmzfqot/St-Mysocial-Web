49a120edd3fde6c23c248dbf48c776b532b2ccdd
Julpa Denaldi <denaldi.j@gmail.com>
2025-03-25 03:01:44 +0700

    change vue-quill to quilljs

--- resources/js/Components/MultiSelect.vue
+++ resources/js/Components/MultiSelect.vue
@@ -1,6 +1,5 @@
 <script setup>
 import {computed} from "vue";
-import {UserPlus} from "lucide-vue-next";
 
 const props = defineProps({
     stUsers: Object,
@@ -33,7 +32,7 @@ const model = computed({
               "tagsInputClasses": "rounded-lg order-1 border-none text-sm dark:bg-neutral-900 dark:placeholder-neutral-500 dark:text-neutral-400",
               "optionTemplate": "<div class=\"flex items-center\"><div class=\"size-8 me-2\" data-icon></div><div><div class=\"text-sm text-gray-800 dark:text-neutral-200 \" data-title></div><div class=\"text-xs text-gray-500 dark:text-neutral-500 \" data-description></div></div><div class=\"ms-auto\"><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-4 text-blue-600\" xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" viewBox=\"0 0 16 16\"><path d=\"M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z\"/></svg></span></div></div>",
               "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"></div>"
-            }' class="hidden ">
+            }' class="hidden">
             <option v-for="user in stUsers" :key="user.id" :value="user.id">{{ user.name }}</option>
         </select>
         <!-- End Select -->
