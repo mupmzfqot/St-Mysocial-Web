776fead8836adf7867069053598d794aa121ff17
Julpa Denaldi <denaldi.j@gmail.com>
2025-05-02 03:32:36 +0700

    generate strong random password

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
      */
     public function store(RegistrationRequest $request)
     {
-        $random_password = rand(); 
+        $random_password = $this->generateStrongPassword();
         $user = User::query()->create([
             'name' => $request->name,
             'username' => $request->username,
@@ -64,8 +64,24 @@ class RegisteredUserController extends Controller
 
     }
 
-    public function payload($request, $random_password)
-    {
-        
+    private function generateStrongPassword($length = 16) {
+        $upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $lower = 'abcdefghijklmnopqrstuvwxyz';
+        $numbers = '0123456789';
+        $symbols = '!@#$%^&*()-_=+[]{}<>?';
+    
+        $all = $upper . $lower . $numbers . $symbols;
+    
+        $password = '';
+        $password .= $upper[random_int(0, strlen($upper) - 1)];
+        $password .= $lower[random_int(0, strlen($lower) - 1)];
+        $password .= $numbers[random_int(0, strlen($numbers) - 1)];
+        $password .= $symbols[random_int(0, strlen($symbols) - 1)];
+    
+        for ($i = 4; $i < $length; $i++) {
+            $password .= $all[random_int(0, strlen($all) - 1)];
+        }
+    
+        return str_shuffle($password);
     }
 }
