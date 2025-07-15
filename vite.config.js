e603df07dbb90aeeeda65e20900540fb1dd49377
Julpa Denaldi <denaldi.j@gmail.com>
2025-07-15 10:43:18 +0700

    update vite to stable version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-// import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
     plugins: [
@@ -18,6 +17,23 @@ export default defineConfig({
                 },
             },
         }),
-        // ...(mode === 'development' ? [vueDevTools()] : []),
     ],
+    build: {
+        outDir: 'public/build',
+        assetsDir: 'assets',
+        manifest: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+            treeshake: {
+                moduleSideEffects: false,
+            },
+        },
+        target: 'es2018',
+    },
+    optimizeDeps: {
+        include: ['vue', '@inertiajs/vue3'],
+        exclude: ['laravel-echo', 'pusher-js'],
+    },
 });
